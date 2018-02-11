8bd6dc699a8fd2a889977accc2b910c10a7e8cfe
seb <harinck.sebastien@gmail.com>
2018-02-11 18:31:27 +0100

    add custom variable in template

--- django/trydjango1-11/src/restaurants/views.py
+++ django/trydjango1-11/src/restaurants/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello")
+    return render(request, 'base.html', {'var': 'my custom variable'})

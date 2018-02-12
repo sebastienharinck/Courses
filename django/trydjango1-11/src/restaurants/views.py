3520596893d83e55e8db219ac25b64c34c57a99b
seb <harinck.sebastien@gmail.com>
2018-02-12 13:02:12 +0100

    add get_context_data

--- django/trydjango1-11/src/restaurants/views.py
+++ django/trydjango1-11/src/restaurants/views.py
@@ -1,10 +1,18 @@
 from django.views.generic import TemplateView
 
+
 class HomeView(TemplateView):
     template_name = 'home.html'
 
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
+
 class AboutView(TemplateView):
     template_name = 'about.html'
+
 
 class ContactView(TemplateView):
     template_name = 'contact.html'

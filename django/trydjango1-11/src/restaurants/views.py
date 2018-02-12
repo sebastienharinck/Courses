23bd179cce1ae248de87fb7a28b3730c56fed9a3
seb <harinck.sebastien@gmail.com>
2018-02-12 10:02:23 +0100

    update views + add smartlook tags

--- django/trydjango1-11/src/restaurants/views.py
+++ django/trydjango1-11/src/restaurants/views.py
@@ -2,6 +2,7 @@ import random
 
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 
 def home(request):
     return render(request, 'home.html', {})
@@ -10,5 +11,13 @@ def about(request):
     return render(request, 'about.html', {})
 
 class ContactView(View):
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
+        print(kwargs)
+        return render(request, 'contact.html', {})
+
+class ContactTemplateView(TemplateView):
+    template_name = 'contact.html'
+
+    def get(self, request, *args, **kwargs):
+        print(kwargs)
         return render(request, 'contact.html', {})

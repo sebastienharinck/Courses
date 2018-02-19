6461ed21cb12640040658617c9301b7e94cbc6e4
seb <harinck.sebastien@gmail.com>
2018-02-19 14:03:03 +0100

    Generic list view

--- django/trydjango1-11/src/muypicky/urls.py
+++ django/trydjango1-11/src/muypicky/urls.py
@@ -17,11 +17,15 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from restaurants.views import restaurant_listview
+from restaurants.views import (
+    restaurant_listview,
+    RestaurantListView,
+)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^restaurants/$', restaurant_listview),
+    url(r'^restaurants/$', RestaurantListView.as_view()),
+    url(r'^restaurants/(?P<slug>\w+)$', RestaurantListView.as_view()),
     url(r'^about/$', TemplateView.as_view(template_name='about.html')),
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html')),
 ]
--- django/trydjango1-11/src/restaurants/views.py
+++ django/trydjango1-11/src/restaurants/views.py
@@ -1,4 +1,6 @@
+from django.db.models import Q
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from .models import Restaurant
 
@@ -11,3 +13,18 @@ def restaurant_listview(request):
 
     }
     return render(request, template_name, context)
+
+
+class RestaurantListView(ListView):
+    template_name = 'restaurants/restaurants_list.html'
+
+    def get_queryset(self):
+        slug = self.kwargs.get('slug')
+        if slug:
+            queryset = Restaurant.objects.filter(
+                Q(category__iexact=slug) |
+                Q(category__icontains=slug)
+            )
+        else:
+            queryset = Restaurant.objects.all()
+        return queryset

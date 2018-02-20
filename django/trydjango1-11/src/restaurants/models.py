172a606eea2ae4405e8fcf8c99153a3150d5fc02
seb <harinck.sebastien@gmail.com>
2018-02-20 17:46:59 +0100

    add User in Restaurant Model

--- django/trydjango1-11/src/restaurants/models.py
+++ django/trydjango1-11/src/restaurants/models.py
@@ -1,11 +1,15 @@
+from django.conf import settings
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 from django.utils.text import slugify
 
 from .validators import validate_category
 
+User = settings.AUTH_USER_MODEL  # to be sure that we import the last user model
+
 
 class Restaurant(models.Model):
+    owner = models.ForeignKey(User)
     name = models.CharField(max_length=120)
     location = models.CharField(max_length=120, null=True, blank=True)
     category = models.CharField(max_length=120, null=True, blank=True, validators=[validate_category])
@@ -20,7 +24,7 @@ class Restaurant(models.Model):
 def restaurant_pre_save_receiver(sender, instance, *args, **kwargs):
     print('saving...')
     if not instance.slug:
-        slug = slugify(instance.name)  # change the attibute to the field that would be used as a slug
+        slug = slugify(instance.name)  # change the attribute to the field that would be used as a slug
         new_slug = slug
         count = 0
         while instance.__class__.objects.filter(slug=new_slug).exclude(id=instance.id).count() > 0:

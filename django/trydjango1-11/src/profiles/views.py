cc2e9874d24d15db8fcc8fe5683ebe994141c09e
seb <harinck.sebastien@gmail.com>
2018-03-08 16:07:20 +0100

    Adding a Robust Search Part 1

--- django/trydjango1-11/src/profiles/views.py
+++ django/trydjango1-11/src/profiles/views.py
@@ -2,6 +2,9 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404, Http404
 from django.views.generic import DetailView
 
+from restaurants.models import Restaurant
+from menus.models import Item
+
 User = get_user_model()
 
 
@@ -13,3 +16,15 @@ class ProfileDetailView(DetailView):
         if username is None:
             raise Http404
         return get_object_or_404(User, username__iexact=username, is_active=True)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProfileDetailView, self).get_context_data(*args, **kwargs)
+        user = context['user']
+        query = self.request.GET.get('q')
+        items_exists = Item.objects.filter(user=user).exists()
+        qs = Restaurant.objects.filter(owner=user)
+        if query:
+            qs = qs.filter(name__icontains=query)
+        if items_exists and qs.exists():
+            context['locations'] = qs
+        return context
